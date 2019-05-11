8fa0d72eac54c3ba62dd318a1826a4b8ee29eb28
William Morgan <billm@wamusa.com>
2019-05-11 18:12:51 -0500

    Inital Commit

---
+++ src/main/java/org/TheFamilyConnection/TheFamilyConnectionApplication.java
@@ -0,0 +1,16 @@
+package org.TheFamilyConnection;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TheFamilyConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFamilyConnectionApplication.class, args);
+//		SpringApplication.run(TheFamilyConnectionApplication.class, args);
+	}
+
+}
