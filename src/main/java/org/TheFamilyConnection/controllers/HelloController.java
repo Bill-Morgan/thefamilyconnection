50d3dfc15b6dc4c7d71ef1ea32b07a8e8b376257
William Morgan <billm@wamusa.com>
2019-05-21 17:02:27 -0500

    trying intellij pro

--- src/main/java/org/TheFamilyConnection/controllers/HelloController.java
+++ src/main/java/org/TheFamilyConnection/controllers/HelloController.java
@@ -1,22 +1,35 @@
 package org.TheFamilyConnection.controllers;
 
+import org.TheFamilyConnection.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("")
+@RequestMapping("lc")
 public class HelloController {
+
+    public static Boolean logedIn = Boolean.FALSE;
 
     @RequestMapping(value="")
     public String index() {
+
+        if (!logedIn) {
+            return ("redirect:login");
+        }
         return "index";
     }
 
     @RequestMapping(value="profile")
     public String displayProfile(Model model){
+
+        if (!logedIn) {
+            return ("redirect:login");
+        }
 
         return ("users/profile");
     }
@@ -31,7 +44,8 @@ public class HelloController {
     public String processLoginForm(@RequestParam String username,@RequestParam String password)  {
 
 
-        if (username.equals("billm") && password.equals("password")) {
+        if (username.equals("billm") && password.equals("password123*")) {
+                logedIn = Boolean.TRUE;
                 return ("redirect:profile");
         }
         return("users/login");
